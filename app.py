d6a42272c00f857265080eec6e23ec8b1069a88d
hiteshi <hitu1@email.com>
2026-05-24 18:10:02 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-API_URL = "https://your-render-url.onrender.com/predict"
+API_URL = "https://mlproject-ehb3.onrender.com"
 
 st.set_page_config(
     page_title="Student Performance Predictor",
